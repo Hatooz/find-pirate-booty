b7b11a7914bd8f72dfd6152151d7cb1495d8f11c
Hatem Mohi El Din <hatem.mohieldin@consid.se>
2023-11-23 08:22:06 +0100

    game skeleton

---
+++ src/app/game/page.tsx
@@ -0,0 +1,128 @@
+import { useRef, useState } from "react";
+
+export type TGame = {
+  id: 17;
+  active: boolean;
+  name: string;
+  currentRoom?: number;
+  gameRooms: TGameRoom[];
+};
+
+export type TGameRoom = {
+  id: number;
+  gameId: number;
+  roomId: number;
+  room: TRoom;
+};
+
+export type TRoom = {
+  id: number;
+  possibelDirections: string;
+};
+
+export default function Game({
+  game,
+  callBack,
+}: {
+  game: TGame;
+  callBack: (id: number) => void;
+}) {
+  const [currentResponse, setCurrentResponse] = useState("");
+  const ref = useRef<HTMLFormElement>(null);
+  const startGame = () => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/gameroom/startgame`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache: "no-store",
+      body: game.id.toString(),
+    }).then((res) => {
+      res.json().then((json) => {
+        // setCurrentReponse(json.content);
+        callBack(game.id);
+      });
+    });
+  };
+  const stopGame = () => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/gameroom/stopgame`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache: "no-store",
+      body: game.id.toString(),
+    }).then((res) => {
+      res.json().then((json) => {
+        // setCurrentReponse(json.content);
+        callBack(game.id);
+      });
+    });
+  };
+
+  const goToDirection = (direction: string) => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/gameroom/nextroom`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache: "no-store",
+      body: JSON.stringify({ direction: direction, gameId: game.id }),
+    }).then((res) => {
+      res.json().then((json) => {
+        // setCurrentReponse(json.content);
+        //   callBack(game.id);
+        console.log(json);
+        if (json.content.toLowerCase().includes("game over")) {
+          alert(json.content);
+        }
+        setCurrentResponse(json.content);
+        reloadGame();
+      });
+    });
+  };
+
+  const reloadGame = () => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/game/${game.id}`, {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache: "no-store",
+    }).then((res) => {
+      res.json().then((json) => {
+        // setCurrentReponse(json.content);
+        //   callBack(game.id);
+        game = json;
+      });
+      callBack(game.id);
+    });
+  };
+  const playerAction = (formData: FormData) => {
+    const playerAction = formData.get("playerAction")?.toString();
+    ref.current?.reset();
+    goToDirection(playerAction as string);
+  };
+  return (
+    <div className="border h-24">
+      {game.name} - {game.active ? "active" : "not active"}-{" "}
+      {!game.active ? (
+        <button onClick={startGame}>Start</button>
+      ) : (
+        <button onClick={stopGame}>Stop</button>
+      )}
+      <div>{game.active && "Game is started"}</div>
+      <div>
+        {game.active && (
+          <div>
+            <form ref={ref} action={playerAction}>
+              <label>In which direction should you proceed?</label>
+              <input type="text" name="playerAction" className="text-black" />
+            </form>
+          </div>
+        )}
+      </div>
+      {currentResponse}
+    </div>
+  );
+}
---
+++ src/app/start/page.tsx
@@ -0,0 +1,133 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+import Game, { TGame } from "../game/page";
+
+export type Room = {
+  id: number;
+  possibleDirections: string;
+};
+
+export default function Start() {
+  const [game, setGame] = useState<TGame>();
+  const [allGames, setAllGames] = useState<TGame[]>();
+  const [gameStarted, setGameStarted] = useState(false);
+  const ref = useRef<HTMLFormElement>(null);
+  const [rerender, setRerender] = useState(false);
+
+  //   useEffect(() => {
+  //     const getRooms = async () => {
+  //       const response = await (
+  //         await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/room`)
+  //       ).json();
+  //       setRooms(response);
+  //     };
+
+  //     getRooms();
+  //   }, [rooms]);
+
+  useEffect(() => {
+    getAllGames();
+  }, [rerender]);
+
+  const reloadPage = () => {
+    setRerender(!rerender);
+  };
+
+  const getAllGames = () => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/game`, {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache: "no-cache",
+    }).then((res) => {
+      res.json().then((json) => {
+        setAllGames(json);
+      });
+    });
+  };
+  const createGame = (bodyData: { name: string; numberOfRooms: number }) => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/game`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bodyData),
+    }).then((res) => {
+      res.json().then((json) => {
+        setGame(json);
+      });
+    });
+  };
+
+  const startCommands = ["game", "start", "begin", "play"];
+
+  const handlePlayerAction = async (formData: FormData) => {
+    ref?.current?.reset();
+
+    const gameName = formData.get("gameName");
+    const numberOfRooms = formData.get("rooms");
+    setGameStarted(true);
+    createGame({
+      name: gameName as string,
+      numberOfRooms: numberOfRooms as unknown as number,
+    });
+
+    // alert("New game started, good luck");
+  };
+
+  //   const startGame = () => {};
+  return (
+    <div className="w-1/2 m-auto mt-20">
+      Welcome to the everchanging pirates dungeon. Can you find my booty?
+      <form
+        ref={ref}
+        action={handlePlayerAction}
+        className="grid grid-cols-1 border border-white"
+      >
+        <label>Game Name:</label>
+        <input
+          type="text"
+          name="gameName"
+          className="bg-black border-b"
+          placeholder="what to call this game?"
+        />
+
+        <label>Number of rooms</label>
+        <input
+          type="number"
+          name="rooms"
+          className="bg-black border-b"
+          placeholder="how many rooms?"
+        />
+
+        <input type="submit" value="Start new game" className="bg-green-950" />
+      </form>
+      <form
+        action={getAllGames}
+        className="grid grid-cols-1 border border-white"
+      >
+        <input
+          type="submit"
+          value="View existing games"
+          className="bg-green-950"
+        />
+      </form>
+      {game && <Game game={game} callBack={reloadPage} />}
+      {allGames && allGames.some((game) => game.active) ? (
+        <Game
+          callBack={reloadPage}
+          game={allGames.find((game) => game.active) as TGame}
+        />
+      ) : (
+        allGames?.map((game) => {
+          return (
+            <div key={game.id}>
+              <Game game={game} callBack={reloadPage} />
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
+}
